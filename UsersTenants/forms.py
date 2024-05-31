fd57ae5c348c185c211379395be3d95a4bed7f8f
Carlos D Martinez <Al234498@alumnos.uacj.mx>
2024-05-30 19:55:12 -0600

    Final 1.04 contra

--- UsersTenants/forms.py
+++ UsersTenants/forms.py
@@ -3,10 +3,20 @@ from .models import *
 from django.contrib.auth.hashers import make_password
 
 class EmpleadoForm(forms.ModelForm):
+    contraseña = forms.CharField(required=False, widget=forms.PasswordInput)
+
     class Meta:
         model = Empleado
         fields = ['nombre', 'apellido', 'posicion', 'contraseña']
 
+    def clean_contraseña(self):
+        # Obtén la contraseña que fue enviada con el formulario
+        contraseña = self.cleaned_data.get('contraseña')
+        # Si hay una nueva contraseña, encriptarla antes de guardarla
+        if contraseña:
+            return make_password(contraseña)
+        # Si no hay contraseña nueva, devuelve None para indicar que no se debe cambiar
+        return None
     
 class PosicionForm(forms.ModelForm):
     class Meta:
