6320ccb77cb6a40112eda69c18d38849faa67938
Carlos D Martinez <Al234498@alumnos.uacj.mx>
2024-05-30 18:39:49 -0600

    Final 1.01942

--- GestorClinicas/urls.py
+++ GestorClinicas/urls.py
@@ -28,6 +28,6 @@ def custom_page_not_found_view(request, exception):
     if request.get_host().split(':')[0] == "gestorclinicasdentales.shop":
         return HttpResponseRedirect("http://gestorclinicasdentales.shop")
     else:
-        return HttpResponseRedirect("/Profile")  # O redirige a la página de inicio de sesión del tenant
+        return HttpResponseRedirect("/")  # O redirige a la página de inicio de sesión del tenant
 
 handler404 = custom_page_not_found_view
